b41aa0ea55b8e12122579501ff08f28a3a753102
christineyu1109 <bqyu@hotmail.com>
2016-10-07 16:34:56 -0400

    Add recent code and draft for report part 2

--- part2/decisiontree.py
+++ part2/decisiontree.py
@@ -4,70 +4,81 @@ import csv
 import collections 
 import re
 import operator
-
-
 import math
+
+#import nltk packages to transform all vocabulary into their root expressions
 from nltk.stem import PorterStemmer
 
-
-
-
+#create empty list to read stop words csv file
 stopword=[]
 
+#read stopwords from csv and save it into stopword list
 with open('stop.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-       # print row
         stopword =stopword+row
 
-numKeywords = 100
-kNearest = 40 #40 not good
-numTotTraining =1000#88639 # 647*137
+#set different values to the following parameters
+
+#number of the most frequent words that will be used as feature words
+numKeywords = 50
+
+#k-nearest neighbors
+kNearest = 3
+
+#total number of examples in training data
+numTotTraining =100#88639 # 647*137
+
+#k-fold cross validation
 kFold = 10
 
-list=[]
-# list to store all abstract as list
+# a single list to store all words in all abstract 
 abstractList =[]
+
+# a single list that will be part of abstractList selected as traning data during the
+# k-fold cross validation process
+# it is used to extract feature words
 allabst=[]
-#accumWords = np.asarray(list)
 
-skip = 1 # to skip first row in training input excel file
+# flag used to skip first row in training input excel file
+skip = 1 
 
-
+# read training abstract
+# remove stopwords
+# and transform each word into its root expression
 with open('train_in.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         if skip == 1:
             skip = 0
         else:
-            #print row
             X = np.array([row])
-            #print X.shape
             keyword = re.findall(r'\w+', X[0,1])
             keywordnew=[x for x in keyword if x not in stopword]
             stemmer = PorterStemmer()
             key_stemmed = [stemmer.stem(kw) for kw in keywordnew]
-            #abstractList[0] is same as keyword
-            abstractList.append(key_stemmed)
+            abstractList.append(keywordnew)
 
-
+#reset the flag for future use
 skip = 1
+
+#create an empty list to store output (class) results
 outputlist =[]
+
+#read train_out csv file and store the info for whole training data into outputlist
 with open('train_out.csv', 'rb') as csvfile:
     readerClasses = csv.reader(csvfile, delimiter=',')
     for row in readerClasses:
         if skip == 1:
             skip = 0
         else:
-            #print row
-            #X is an array shape 1,2
             X = np.array([row])
-            #print X[0,1]
+
             outputlist.append(X[0,1])
-    #print outputlist # is list of size number of instances
-
-
-#Create several empty arrays for storing training/testing inputs/outputs with k-fold cross validation
+ # if print: outputlist is a list of of size = number of instances
+ 
+# create several empty arrays for storing training/testing inputs/outputs for each 
+# round of k-fold cross validation
 emp=[]
 trainingX=np.asarray(emp)
 testingX =np.asarray(emp)
@@ -75,74 +86,64 @@ trainingY=[]
 testingY =[]
 
 numPerSet = numTotTraining/kFold
-#implement k fold 
-# Initialize variable to store error value
+# initialize variable to store average error value at the end of cross validation
 avgError = 0
 
-print len(outputlist)
+#print len(outputlist)
+# start to implement k fold  cross validation
 for index in range(0,kFold):
-    print 
+    
+    # store outputs of training data and combine abstract of all training data into allabst[]  
     for j in range(0,index*numPerSet):
         allabst=allabst+abstractList[j]
         trainingY.append(outputlist[j])
        
-    
+    # store outputs of training data
     for j in range((index+1)*numPerSet,kFold*numPerSet):
         allabst=allabst+abstractList[j]
         trainingY.append(outputlist[j])
-    
-
-    #allabst_2 = [allabst_2 + stemmer.stem(kw) for kw in allabst]
-    #print 'allabst'
-    #print allabst
-    #print 'allabst_2'
-    #print allabst_2
    
-    #allabst_2 is list with root ['roor','rrr']
+    #find out the most frequent words in allabst[] and store in idx[] as feature words
     wordFreq = collections.Counter(allabst)
     mostFreq = wordFreq.most_common(numKeywords)
     print mostFreq
     c = np.array(mostFreq)
     idx = np.unique(c[:,0].reshape((numKeywords,1)))
- 
+    
+    #trainingX is of row size = idx, and col size = number of training data in this round of k-fold
     trainingX = np.zeros((idx.shape[0],numPerSet*(kFold-1)))
+
     for j in range(0,index*numPerSet):
         for i in range(idx.shape[0]):
         #abstractList is a list with j element
             if idx[i] in abstractList[j]:
-                trainingX[i][j] =  abstractList[j].count(idx[i])#30
-            #elif idx[i] in abstractList[j] and abstractList[j].count(idx[i])<=3 and abstractList[j].count(idx[i])>1:
-            #    trainingX[i][j] = 30
-            #elif idx[i] in abstractList[j] and abstractList[j].count(idx[i])<=3 and abstractList[j].count(idx[i])>1:
-            #    trainingX[i][j] = 30
+                # if the word idx[] is present in this abstract of person j
+                # the corresponding position in trainingX[i][j] will no longer to be zero
+                trainingX[i][j] =  abstractList[j].count(idx[i])
    
     for j in range((index+1)*numPerSet,kFold*numPerSet):
         for i in range(idx.shape[0]):
         #abstractList is a list with j element
             if idx[i] in abstractList[j]:
+                # if the word idx[] is present in this abstract of person j
+                # the corresponding position in trainingX[i][j] will no longer to be zero
                 trainingX[i][j-numPerSet] =  abstractList[j].count(idx[i])#30
-            #elif idx[i] in abstractList[j] and abstractList[j].count(idx[i])<=3 and abstractList[j].count(idx[i])>1:
-            #    trainingX[i][j-numPerSet] = 30
-            #elif idx[i] in abstractList[j] and abstractList[j].count(idx[i])==1:
-            #    trainingX[i][j-numPerSet] = 30
-    #finish construct trainingX trainingY for this k fold round
-    #Based on cross validation number, store necessary training/testing inputs/outputs info for each round of cross validation
+
     testingX = np.zeros((idx.shape[0],numPerSet))
     for j in range(index*numPerSet,(index+1)*numPerSet):
         for i in range(idx.shape[0]):
         #abstractList is a list with j element
             if idx[i] in abstractList[j]:
+                # if the word idx[] is present in this abstract of person j
+                # the corresponding position in testing[i][j] will no longer to be zero
                 testingX[i][j-index*numPerSet] =  abstractList[j].count(idx[i])
-            #elif idx[i] in abstractList[j] and abstractList[j].count(idx[i])<=3 and abstractList[j].count(idx[i])>1:
-            #    testingX[i][j-index*numPerSet] = 30
-            #elif idx[i] in abstractList[j] and abstractList[j].count(idx[i])==1:
-            #    testingX[i][j-index*numPerSet] = 30
-
-
+        #construct testing set output data
         testingY.append(outputlist[j])
 
     curError =0
-    for i in range (0,testingX.shape[1]):#range(0,testingX.shape[1]):
+
+    #for each sample in testing data, find out their nearest neighbors
+    for i in range (0,testingX.shape[1]):
         distance =[]
         for j in range(0,trainingX.shape[1]):
             diff = testingX[:,i]- trainingX[:,j]
@@ -151,41 +152,32 @@ for index in range(0,kFold):
         distance.sort(key=operator.itemgetter(1))
         neighbors =[]
         for x in range(kNearest):
-            #print "distance"
-            #print distance
             neighbors.append(distance[x][0])
-        #print 'neigh for people# {}'.format(i)
-        #print neighbors # the idex are number of j; col index in current training set nearest the testing instance i
-    
-        # grap trainng output classes to see average
-        nearestClass =[] # put classes of all neigbour together to get average
+        # put classes of all neigbour together to get average
+        nearestClass =[] 
         for neibIdx in range(kNearest):
             tempIdx = neighbors[neibIdx]
             nearestClass.append(trainingY[tempIdx])
-        #get for current i for testingX the neigbours outputs list
-        #print 'for testing idx {} neigb outpus list is'.format(i)
-        #print nearestClass
+        # find out the majority class from the classes of all its neighbors
         countNear = collections.Counter(nearestClass)
         majorClassList = countNear.most_common(1)
         majorClassArr = np.array(majorClassList)
         majorClass=majorClassArr[0,0]
         #print 'predic Class {}'.format(majorClass)
-        #findout real testingX i class output
+
         realClass=testingY[i]
         #print 'true Class {}'.format(realClass)
         #print majorClass==realClass
-        #to save the error for this k fold round
-        
-
+     
+        #if no match: error increment by 1
         if majorClass!=realClass:
             curError += 1
-    #give the rate of error (by dividing total curError number by number of testing set)
-    #print 'testingX.shape[1] is {}'.format(testingX.shape[1])
+    #give the rate of error (by dividing the total curError number by the total number of testing set)
     print 'curError is {}'.format(curError)
     curKFoldRoundErrorRate = 0.1*curError/testingX.shape[1]/0.1
     print 'curKFoldRoundErrorRate is {}'.format(curKFoldRoundErrorRate)
     avgError += curKFoldRoundErrorRate
-
+    #clear up the arrays for next round of k-fold validation
     trainingX=np.asarray(emp)
     testingX =np.asarray(emp)
     trainingY=[]
@@ -195,34 +187,7 @@ print'average arror after k is'
 avgError /=  kFold
 print avgError
 
-with open('mydata.csv', 'w') as mycsvfile:
-    thedatawriter = csv.writer(mycsvfile, delimiter=' ')
-    thedatawriter.writerow(avgError)
 
- #   for x in range(len(trainingSet)):
-	#	dist = euclideanDistance(testInstance, trainingSet[x], length)
-	#	distances.append((trainingSet[x], dist))
-	#distances.sort(key=operator.itemgetter(1))
-
-
-
-        #def sumSquareDiff(self,X,Y, weights):
-        #theoryY = np.dot(X, weights)
-        #print('The predicted y is')
-        #print(theoryY)
-        #diff = (Y-theoryY)
-        #sumSquareDiffNum = np.sum(np.square(diff))
-        #return sumSquareDiffNum
-    
-    #for x in np.nditer(idx):
-    #    print x
-
-
-        #see = np.array([['xxx']['xxx']['xxx']['yyy']['yyy']['g']])
-        #print see
-        #print see.shape
-        #vvv = np.vstack({tuple(row) for row in see})
-        #print vvv
 
 
 
